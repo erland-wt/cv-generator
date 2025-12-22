40ade8a73e57f3b8b52bdda6701bbbd2a1bc56fd
erland-wt <erlandwidyatamaka05@gmail.com>
2025-12-23 00:32:40 +0700

    Homepage

--- src/components/CVTemplate.tsx
+++
@@ -1,7 +0,0 @@
-export default function CVTemplate1({ text }: { text: string }) {
-  return (
-    <div className="p-8 border border-gray-300 rounded-lg">
-      <pre className="whitespace-pre-wrap">{text}</pre>
-    </div>
-  );
-}
